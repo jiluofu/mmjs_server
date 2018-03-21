32308670a2c04f4c62558b329026a4dc63ef717a
jiluofu <jiluofu@gmail.com>
2018-03-21 09:52:14 +0800

    =update

--- sh/shell_down_weather.sh
+++ sh/shell_down_weather.sh
@@ -1,4 +1,4 @@
-# 下载交通台早上一路畅通，截取天气部分00:31:30-00:36:30
+# 下载交通台早上一路畅通，截取天气部分00:31:50-00:36:30
 # http://playback.rbc.cn/audioD/fm1039/20180319/0730007200_mp4/073000_7200_96K.mp4
 
 if [ "$1" = "" ];then
@@ -18,7 +18,7 @@ wget -O $output/$name $url
 weather_name=$time"_weather.mp4"
 echo $weather_name
 
-ffmpeg -y -i $output/$name -ss 00:31:30 -to 00:36:30 -acodec copy $output/$weather_name
+ffmpeg -y -i $output/$name -ss 00:31:50 -to 00:36:30 -acodec copy $output/$weather_name
 rm -rf $output/$name
 
 weather_file_name=$time"_专家聊天气.mp3"
